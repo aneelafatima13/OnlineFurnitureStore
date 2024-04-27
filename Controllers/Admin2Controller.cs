fff9c1ef17e8c014e17c122a79d7094a1ba89b23
aneelafatima13 <aneelafatimacst@gmail.com>
2024-04-28 02:21:27 +0500

    Works on Reports Generation

--- Controllers/Admin2Controller.cs
+++ Controllers/Admin2Controller.cs
@@ -40,12 +40,16 @@ namespace OnlineFurnitureStore.Controllers
 
         public ActionResult DailySaleReportbyProduct()
         {
+            List<Tbl_Product> products = db.Tbl_Product.ToList();
+            ViewBag.Products = products;
             var cartProductDetails = db.Tbl_Cart.ToList();
             return View(cartProductDetails);
         }
 
         public ActionResult DailySaleReportbyMembers()
         {
+            List<Tbl_Members> members = db.Tbl_Members.ToList(); // Replace YourDAL with your actual DAL class
+            ViewBag.Members = members;
             var cartProductDetails = db.Tbl_Cart.ToList();
             return View(cartProductDetails);
         }
@@ -104,6 +108,74 @@ namespace OnlineFurnitureStore.Controllers
 
             return Json(cartDetails, JsonRequestBehavior.AllowGet);
         }
+        public ActionResult GetCartDetailsByMember(int member)
+        {
+            // Filter cart details based on the selected city
+            var cartDetails = db.Tbl_Cart
+                                .Where(c => c.Tbl_ShippingDetails.MemberId == member)
+                                .Select(c => new
+                                {
+                                    
+                                    MemberName = c.Tbl_Members.FirstName + " " + c.Tbl_Members.LastName,
+                                    ProductName = c.Tbl_Product.ProductName,
+                                    CartProductQuantity = c.CartProductQuantity,
+                                    CartProductTotalPrice = c.CartProductTotalPrice,
+                                    CartDate = c.CartDate,
+                                    Address = c.Tbl_ShippingDetails.Adress,
+                                    City = c.Tbl_ShippingDetails.City,
+                                    Country = c.Tbl_ShippingDetails.Country,
+                                    AmountPaid = c.Tbl_ShippingDetails.AmountPaid,
+                                    PaymentType = c.Tbl_ShippingDetails.PaymentType
+                                })
+                                .ToList();
+
+            return Json(cartDetails, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetCartDetailsByProduct(int product)
+        {
+            // Filter cart details based on the selected city
+            var cartDetails = db.Tbl_Cart
+                                .Where(c => c.ProductId == product)
+                                .Select(c => new
+                                {
+                                    CartDate = c.CartDate,
+                                    ProductName = c.Tbl_Product.ProductName,
+                                    CartProductQuantity = c.CartProductQuantity,
+                                    CartProductTotalPrice = c.CartProductTotalPrice,
+                                    MemberName = c.Tbl_Members.FirstName + " " + c.Tbl_Members.LastName
+
+                                })
+                                .ToList();
+
+            return Json(cartDetails, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetCartDetailsByStatus(int status)
+        {
+            // Filter cart details based on the selected city
+            var cartDetails = db.Tbl_Cart
+                                .Where(c => c.CartStatusId == status)
+                                .Select(c => new
+                                {
+                                    CartStatus = c.Tbl_CartStatus.CartStatus,
+                                    MemberName = c.Tbl_Members.FirstName + " " + c.Tbl_Members.LastName,
+                                    ProductName = c.Tbl_Product.ProductName,
+                                    CartProductQuantity = c.CartProductQuantity,
+                                    CartProductTotalPrice = c.CartProductTotalPrice,
+                                    CartDate = c.CartDate,
+                                    Address = c.Tbl_ShippingDetails.Adress,
+                                    City = c.Tbl_ShippingDetails.City,
+                                    Country = c.Tbl_ShippingDetails.Country,
+                                    AmountPaid = c.Tbl_ShippingDetails.AmountPaid,
+                                    PaymentType = c.Tbl_ShippingDetails.PaymentType
+                                })
+                                .ToList();
+
+            return Json(cartDetails, JsonRequestBehavior.AllowGet);
+        }
+
+
         public ActionResult DailySaleReportbyPaymentType()
         {
             var cartProductDetails = db.Tbl_Cart.Where(c => c.Tbl_ShippingDetails.PaymentType == "CashOnDelivery").ToList();
@@ -111,6 +183,8 @@ namespace OnlineFurnitureStore.Controllers
         }
         public ActionResult DailySaleReportbyCartStatus()
         {
+            List<Tbl_CartStatus> status = db.Tbl_CartStatus.ToList(); // Replace YourDAL with your actual DAL class
+            ViewBag.CartStatus = status;
             var cartProductDetails = db.Tbl_Cart.Where(c => c.CartStatusId == 1).ToList();
             return View(cartProductDetails);
         }
@@ -141,6 +215,12 @@ namespace OnlineFurnitureStore.Controllers
                                         .ToList();
 
             return View(cartProductDetails);
+        }
+
+        public ActionResult GetMembersList()
+        {
+            var members = db.Tbl_Members.ToList();
+            return Json(members, JsonRequestBehavior.AllowGet);
         }
 
     }
